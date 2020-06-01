21e8f6fd8c58511e6ee56a473d367d8b897c0b4b
Djalal Harouni <tixxdz@opendevices.io>
2020-06-02 00:23:56 +0100

    deploy: use deploy.bash
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

--- deploy.bash
+++ deploy.bash
@@ -1,7 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # abort on errors
 set -e
+
+# install
+npm install
 
 # build
 npm run build
@@ -9,8 +12,10 @@ npm run build
 # navigate into the build output directory
 cd .vuepress/dist
 
-# if you are deploying to a custom domain
-echo 'docs.ionoid.io' > CNAME
+if [ ! -f CNAME ]; then
+        # if you are deploying to a custom domain
+        echo 'docs-dev.ionoid.io' > CNAME
+fi
 
 git init
 git add -A
@@ -20,6 +25,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ionoid/docs.ionoid.io.git master:gh-pages
+git push -f git@github.com:ionoid/docs-dev.ionoid.io.git master:gh-pages
 
 cd -
