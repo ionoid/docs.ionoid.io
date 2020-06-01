3c9879a32f11ea4acd2d0e45c5f01544c5a55fab
Djalal Harouni <tixxdz@opendevices.io>
2020-06-02 00:48:03 +0100

    docs: lets use docs-dev for now
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

--- deploy.bash
+++ deploy.bash
@@ -14,7 +14,7 @@ cd .vuepress/dist
 
 if [ ! -f CNAME ]; then
         # if you are deploying to a custom domain
-        echo 'docs.ionoid.io' > CNAME
+        echo 'docs-dev.ionoid.io' > CNAME
 fi
 
 git init
